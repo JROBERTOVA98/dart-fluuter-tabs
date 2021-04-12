367fda15a8aed85338643bf28f560cc4aa2a1934
Roberto Vargas <jrobertova98@gmail.com>
2021-04-12 16:35:29 -0500

    Primer commit

---
+++ lib/src/screens/contact.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Contact extends StatelessWidget {
+  const Contact({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: <Widget>[
+        Icon(Icons.contacts, size: 170, color: Colors.grey),
+        Text("Tercer Tab")
+      ],
+    ));
+  }
+}
